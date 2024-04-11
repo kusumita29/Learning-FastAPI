79316cfa51c4c1ae590ce9556dd153ce60540410
Kusumita <29kusumita@gmail.com>
2024-04-11 23:52:04 +0530

    fixed errors

--- router/article.py
+++ router/article.py
@@ -2,19 +2,28 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 from db.database import get_db
 from db import db_article
-from schemas import ArticleBase, ArticleDisplay
+from schemas import ArticleBase, ArticleDisplay, UserBase
 from typing import List
+from auth.oauth2 import get_current_user, oauth2_scheme
 
 router = APIRouter(prefix="/article", tags=["article"])
 
 
 # create article
 @router.post("/", response_model=ArticleDisplay)
-def create_article(request: ArticleBase, db: Session = Depends(get_db)):
+def create_article(
+    request: ArticleBase,
+    db: Session = Depends(get_db),
+    current_user: UserBase = Depends(get_current_user),
+):
     return db_article.create_article(db, request)
 
 
 # get specific article by its id
 @router.get("/{id}", response_model=ArticleDisplay)
-def get_article(id: int, db: Session = Depends(get_db)):
+def get_article(
+    id: int,
+    db: Session = Depends(get_db),
+    current_user: UserBase = Depends(get_current_user),
+):
     return db_article.get_article(db, id)

a6731662310a994528c1dc80b5b34c9b081344c6
Kusumita <29kusumita@gmail.com>
2024-04-11 23:07:27 +0530

    authentication

--- db/db_user.py
+++ db/db_user.py
@@ -22,11 +22,12 @@ def get_all_users(db: Session):
     return db.query(DbUser).all()
 
 
-def get_user_by_id(db: Session, id: int):
-    user = db.query(DbUser).filter(DbUser.id == id).first()
+def get_user_by_username(db: Session, username: str):
+    user = db.query(DbUser).filter(DbUser.username == username).first()
     if not user:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id {id} not found"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"User with username {username} not found",
         )
     return user
 
